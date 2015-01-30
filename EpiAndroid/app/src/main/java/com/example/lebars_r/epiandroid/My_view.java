65f603235ecb18da828d9214eb9a4b43df3106b4
lebars-r <romain.lebars@epitech.eu>
2015-01-30 21:12:28 +0100

    class view

---
+++ EpiAndroid/app/src/main/java/com/example/lebars_r/epiandroid/My_view.java
@@ -0,0 +1,12 @@
+package com.example.lebars_r.epiandroid;
+
+import android.widget.TextView;
+
+/**
+ * Created by lebars_r on 30/01/2015.
+ */
+public class My_view {
+    public void put_data_in_view(TextView _view, String _data){
+        _view.setText(_data);
+    }
+}
